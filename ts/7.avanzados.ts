48af0e66919b95152cee0d0f4aace407e9c1731c
Juan Ruiz <jdruiz008@gmail.com>
2025-06-09 21:53:42 -0500

    ejercicio t()

--- ts/7.avanzados.ts
+++ ts/7.avanzados.ts
@@ -42,4 +42,50 @@ function getData<T>(person:T, key: keyof T) {
 }
 
 const getData1 = getData<IStudent1>(harry,'dni')
+const getData2 = getData(dumbeldore,'career')
 console.log(getData1)
+
+const es = {
+    home: {
+        header: {
+            buttonProfile: {
+                button1: 'Acceder al perfil',
+                button2: {
+                    logout: 'salir',
+                    login: 'entrar'
+                }
+            },
+            buttonLogout: 'Cerrar sesion'
+        }
+    }
+} as const
+
+function t(path:string, obj:{}):unknown{
+    const pathSplit = path.split('.')
+
+    const pathValue = pathSplit.reduce((acumulador, valorActual) =>{
+        return acumulador[valorActual]
+    },obj)
+    return pathValue
+    
+}
+console.log(t('home.header.buttonProfile.button2.logout',es))
+
+//version mas sencilla
+
+type DeepKeys<T> = T extends Record<string,any>
+? {
+    [k in keyof T]: k extends string
+    ?  T[k] extends Record<string,any>
+        ? `${k}` | `${k}.${DeepKeys<T[k]>}`
+        : `${k}`
+    :never
+}[keyof T]
+:never;
+type keysPath  = DeepKeys<typeof es>
+
+function x<T extends Record<string,any>>(path:DeepKeys<typeof es>, obj:{}):unknown{
+    return path.split('.').reduce((acc, curr) => {return acc[curr]},obj)
+}
+
+console.log(x('home.header.buttonProfile.button2.logou',es))
